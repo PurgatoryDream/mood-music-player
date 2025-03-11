f799ac40596e2a62c30797a201875b0a26b61e9c
PurgatoryDream <purgatorydreaming@gmail.com>
2025-03-11 20:15:42 +0100

    Adding some style and designing the playlist preview portion.

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
 # mood-music-player
 
-This template should help get you started developing with Vue 3 in Vite.
-
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
-
-## Type Support for `.vue` Imports in TS
-
-TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vite.dev/config/).
+This is a project meant for practice with Vue.js and TypeScript, to make a single-page application that manages several playlists of songs and then allows the user to play them (and switch between them through the use of keybinds)! I'm also using vue-router to manage the application's different routes. Feel free to play around with this repository as much as you want to.
 
 ## Project Setup
 
--- src/components/playlistedit/PlaylistPreview.vue
+++ src/components/playlistedit/PlaylistPreview.vue
@@ -8,9 +8,18 @@
         <span>Test</span>
       </div>
     </div>
-    <div class="playlist-preview-form-item">
-      <label for="playlist-name">Playlist Name:</label>
-      <input type="text" id="playlist-name" name="plname">
+    <div class="playlist-preview-form-container">
+      <div class="playlist-preview-form-item">
+        <label for="playlist-name">Playlist Name:</label>
+        <input type="text" id="playlist-name" name="plname">
+      </div>
+      <div class="playlist-preview-form-item">
+        <label for="playlist-color">Color:</label>
+        <input type="color" id="playlist-color" name="plcolor">
+      </div>
+      <div class="playlist-preview-form-submit">
+        <button type="submit">SAVE!</button>
+      </div>
     </div>
   </div>
 </template>
@@ -23,6 +32,9 @@
   width: 100%;
   height: 100%;
   overflow: hidden;
+
+  border-right: 1px solid black;
+  box-shadow: 1px 1px 2px 4px black;
 }
 
 .playlist-preview-icon-container {
@@ -34,20 +46,69 @@
 }
 
 .playlist-preview-icon {
-  width: 50%;
-  height: 50%;
+  width: 15vw;
+  min-width: 200px;
+  height: 15vw;
+  min-height: 200px;
+
   display: flex;
   justify-content: center;
   align-items: center;
 
   border: 1px solid black;
+  box-shadow: 1px 1px 2px black;
   border-radius: 25%;
 }
 
-.playlist-preview-form-item {
-  height: 30%;
+.playlist-preview-form-container {
+  width: 90%;
+  height: 60%;
+  margin: auto;
   display: flex;
+  flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
+}
+
+.playlist-preview-form-item {
+  width: 100%;
+  height: 25%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+
+.playlist-preview-form-item label {
+  font-size: 16px;
+  height: 20%;
+}
+
+.playlist-preview-form-item input[type=text] {
+  border-radius: 4px;
+  padding: 8px 10px;
+  width: 100%;
+}
+
+.playlist-preview-form-item input[type=color] {
+  border-radius: 4px;
+  width: 100%;
+  height: 20%;
+}
+
+.playlist-preview-form-submit {
+  width: 100%;
+  height: 25%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: auto;
+}
+
+.playlist-preview-form-submit button {
+  width: 70%;
+  height: 50%;
+  font-size: 20px;
 }
 </style>
